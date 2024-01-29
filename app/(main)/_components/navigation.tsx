7d642783001a9a0abae6c213fed07f2ee228b5de
alguadmar <alguadmar@gmail.com>
2024-01-29 13:20:01 -0600

    Added user dropdown in Documents page

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useRef, ElementRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import UserItem from "./user-item";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -99,7 +100,7 @@ export const Navigation = () => {
                 <ChevronsLeft className="h-6 w-6"/>
             </div>
             <div>
-                <p>Action Items</p>
+               <UserItem/>
             </div>
             <div className="mt-4">
                 Documents 
